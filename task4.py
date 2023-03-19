6bf7a3643cf4d2e288e68a5ec9ebb2698a359dde
Соня <s.zlaya@g.nsu.ru>
2023-03-19 20:52:11 +0700

    Сделаны задания семинара 4

--- task4.py
+++
@@ -1,6 +0,0 @@
-string = input('Введите слово: ')
-string_reversed = reversed(string)
-if "yes" == "".join(set(["yes" if i == j else "no" for i, j in zip(string, string_reversed)])):
-    print("yes")
-else:
-    print("no")
