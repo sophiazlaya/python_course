1968d0b166794be74b5a7be4131b1a4d31bf2471
Соня <s.zlaya@g.nsu.ru>
2023-06-02 23:53:49 +0700

    задания семинара 5, переделанное 1

--- task_5/task_5.1.py
+++ task_5/task_5.1.py
@@ -7,7 +7,7 @@ with open('stage3_test.csv', 'r', encoding='utf-8') as a:
     for word in csvreader:
         if word[2]:
             counter[word[2]] += 1
-            max = counter.most_common(20)
-            min = counter.most_common()[-21:-1]
-            print('20 the most frequent: ', max)
-            print('20 the rarest: ', min)
+    max = counter.most_common(20)
+    min = counter.most_common()[-21:-1]
+    print('20 the most frequent: ', max)
+    print('20 the rarest: ', min)
