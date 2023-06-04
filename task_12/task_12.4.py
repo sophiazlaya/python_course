a5c090ec5f31e0b5ce44fde4eb11543c4bd8a910
Соня <s.zlaya@g.nsu.ru>
2023-06-04 23:45:10 +0700

    задание task_12.4 исправлено

--- task_12/task_12.4.py
+++ task_12/task_12.4.py
@@ -11,21 +11,21 @@ class Library:
         if password == library_admission:
             print('number of copies in the library:', self.__num_of_copies, '\n', 'owners:', self.__owner)
         else:
-            print('Wrong password')
+            return 'Wrong password'
 
     def get_info(self):
         print('author:', self.author, '\n', 'title:', self.title)
         self.__get_full_info(input('Enter the password for full admission: '))
 
-    def set_NumOfCop(self):
+    def set_num_of_cop(self):
         self.__num_of_copies -= 1
 
     def get_book(self, owner):
         if self.__num_of_copies >= 1:
-            self.set_NumOfCop()
+            self.set_num_of_cop()
             self.__owner.append(owner)
 
-    def set_NewOwner(self, name1, name2):
+    def set_new_owner(self, name1, name2):
         if (name1 != name2) and (name2 not in self.__owner):
             self.__owner[self.__owner.index(name1)] = name2
 
@@ -34,5 +34,5 @@ book1 = Library('Ыix of ravens', 'Love&hate', 3, [])
 book1.get_book('Eva Evans')
 book1.get_book('Ann Jane')
 book1.get_info()
-book1.set_NewOwner('Ann Jane', 'Alice')
+book1.set_new_owner('Ann Jane', 'Alice')
 book1.get_info()
